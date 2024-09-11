8bc8a46a43a4d6c3d39586d7891dc4ea709453de
Nada Hassan <nadahassanhossini@gmail.com>
2024-09-11 12:34:41 +0300

    adding (post - put) service

--- Waddabha.API/Controllers/ServicesController.cs
+++ Waddabha.API/Controllers/ServicesController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Waddabha.API.ResponseModels;
+using Waddabha.BL.DTOs.Categories;
+using Waddabha.BL.DTOs.Services;
 using Waddabha.BL.Managers.Services;
+using Waddabha.DAL.Data.Models;
 
 namespace Waddabha.API.Controllers
 {
@@ -34,6 +38,20 @@ namespace Waddabha.API.Controllers
             var service = _serviceManager.GetById(id);
             _serviceManager.Delete(service.Id);
             return NoContent();
+        }
+        [HttpPost]
+        public IActionResult Add(ServiceAddDTO serviceAddDTO)
+        {
+            var service = _serviceManager.Add(serviceAddDTO);
+            var response = ApiResponse<ServiceReadDTO>.SuccessResponse(service);
+            return Ok(response);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id,ServiceUpdateDTO serviceUpdateDTO)
+        {
+            var service = _serviceManager.Update(id, serviceUpdateDTO);
+            var response = ApiResponse<ServiceReadDTO>.SuccessResponse(service);
+            return Ok(response);
         }
     }
 }
