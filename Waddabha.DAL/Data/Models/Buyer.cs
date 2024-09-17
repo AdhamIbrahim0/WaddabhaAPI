16e435c734052bbddab2122b98942b20fb076f69
mohamedAyman00 <ma1090560@gmail.com>
2024-09-17 19:44:24 +0300

    Fix Contract Cycle

--- Waddabha.DAL/Data/Models/Buyer.cs
+++ Waddabha.DAL/Data/Models/Buyer.cs
@@ -1,7 +1,10 @@
-﻿namespace Waddabha.DAL.Data.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Waddabha.DAL.Data.Models
 {
     public class Buyer : User
     {
+        [JsonIgnore]
         public virtual ICollection<Contract>? Contracts { get; set; }
         public virtual ICollection<ChatRoom>? ChatRooms { get; set; }
     }
